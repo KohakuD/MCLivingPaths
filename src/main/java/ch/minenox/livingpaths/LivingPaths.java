5aec55e292cb8cdc7ddd1dc4199b0e16d8bcd2eb
David <noahsameli@gmail.com>
2026-08-10 23:30:05 +0200

    Initial NeoForge 1.21.1 project setup

---
+++ src/main/java/ch/minenox/livingpaths/LivingPaths.java
@@ -0,0 +1,12 @@
+package ch.minenox.livingpaths;
+
+import net.neoforged.fml.common.Mod;
+
+@Mod(LivingPaths.MOD_ID)
+public final class LivingPaths {
+
+    public static final String MOD_ID = "livingpaths";
+
+    public LivingPaths() {
+    }
+}
