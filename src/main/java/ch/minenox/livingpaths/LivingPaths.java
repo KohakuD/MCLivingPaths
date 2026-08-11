fb1d332b31ea8ce5cd3f8552b5ae3be6c387b9a3
ID7ZH <noahsameli@gmail.com>
2026-08-11 04:45:39 +0200

    Align config registration with NeoForge lifecycle

--- src/main/java/ch/minenox/livingpaths/LivingPaths.java
+++ src/main/java/ch/minenox/livingpaths/LivingPaths.java
@@ -1,7 +1,6 @@
 package ch.minenox.livingpaths;
 
 import ch.minenox.livingpaths.config.LivingPathsConfig;
-import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.fml.config.ModConfig;
@@ -11,7 +10,7 @@ public final class LivingPaths {
 
     public static final String MOD_ID = "livingpaths";
 
-    public LivingPaths(IEventBus modEventBus, ModContainer modContainer) {
+    public LivingPaths(ModContainer modContainer) {
         modContainer.registerConfig(ModConfig.Type.COMMON, LivingPathsConfig.SPEC);
     }
 }
