3c32ad3155785b2e3f67378345931e1fd8cfc07c
ID7ZH <noahsameli@gmail.com>
2026-08-11 04:44:52 +0200

    Register Living Paths common configuration

--- src/main/java/ch/minenox/livingpaths/LivingPaths.java
+++ src/main/java/ch/minenox/livingpaths/LivingPaths.java
@@ -1,12 +1,17 @@
 package ch.minenox.livingpaths;
 
+import ch.minenox.livingpaths.config.LivingPathsConfig;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.config.ModConfig;
 
 @Mod(LivingPaths.MOD_ID)
 public final class LivingPaths {
 
     public static final String MOD_ID = "livingpaths";
 
-    public LivingPaths() {
+    public LivingPaths(IEventBus modEventBus, ModContainer modContainer) {
+        modContainer.registerConfig(ModConfig.Type.COMMON, LivingPathsConfig.SPEC);
     }
 }
